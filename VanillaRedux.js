4268614a4dd93c55b9bda1a06ab56e1b6b485e28
Colson <colsoncalifornication@gmail.com>
2024-03-21 15:59:45 +0545

    implement claim history, accounting and policy reducers

--- VanillaRedux.js
+++ VanillaRedux.js
@@ -30,3 +30,34 @@ const createClaim = (name, amountOfMoneyToCollect) => {
     },
   };
 };
+
+// Reducers (Departments!)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    // we care about this action (FORM!)
+    return [...oldListOfClaims, action.payload];
+  }
+
+  //we don't care about the action (form!!)
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amountOfMoneyToCollect;
+  }
+
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === 'CREATE_POLICY') {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === 'DELETE_POLICY') {
+    return listOfPolicies.filter((name) => name !== action.payload.name);
+  }
+
+  return listOfPolicies;
+};
